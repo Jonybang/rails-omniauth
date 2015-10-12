bbe55f36dfc802c302c09a684d865e6226f63861
jonybang <jonybang@mail.ru>
2015-10-12 20:26:36 +1000

    deploy configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :user,            'deploy'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
-set :pg_user, 'deploy'
+#set :pg_user, 'deploy'
 
 # Don't change these unless you know what you're doing
 set :pty,             true
